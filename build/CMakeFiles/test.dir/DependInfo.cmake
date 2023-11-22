a16309cdc516c2e8b7c48937a45476b09641b2f9
Tendo1904 <71323590+Tendo1904@users.noreply.github.com>
2023-11-22 14:04:17 +0300

    build added

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/artem/Some stuff/brawler/main.cpp" "CMakeFiles/test.dir/main.cpp.obj" "gcc" "CMakeFiles/test.dir/main.cpp.obj.d"
+  "C:/Users/artem/Some stuff/brawler/src/battle.cpp" "CMakeFiles/test.dir/src/battle.cpp.obj" "gcc" "CMakeFiles/test.dir/src/battle.cpp.obj.d"
+  "C:/Users/artem/Some stuff/brawler/src/characters.cpp" "CMakeFiles/test.dir/src/characters.cpp.obj" "gcc" "CMakeFiles/test.dir/src/characters.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
